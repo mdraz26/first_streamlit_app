1abba115dc832307841961a910c1c35f34bf1e0d
mdraz26 <105057959+mdraz26@users.noreply.github.com>
2023-06-15 12:23:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+
+streamlit.header('Breajfast Menu')
+streamlit.text('Vitalis')
+streamlit.text('Bacon & eggs')
+streamlit.text('Oatmeal')
