a90bb91748e47b8bddcf2f9ecd0b1148c663a3b1
mdraz26 <105057959+mdraz26@users.noreply.github.com>
2023-06-15 12:24:30 +0200

    Update streamlit_app.py
    fixed menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
 
-streamlit.header('Breajfast Menu')
-streamlit.text('Vitalis')
-streamlit.text('Bacon & eggs')
-streamlit.text('Oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
