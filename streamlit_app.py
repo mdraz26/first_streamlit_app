81826c1f6ff51340b643ae1993e87c857b07c26f
mdraz26 <105057959+mdraz26@users.noreply.github.com>
2023-06-16 14:09:38 +0200

    Update streamlit_app.py
    maknut json prikaz podataka

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-# 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# ispravlja strukturu json-a??
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# prikazuje podatke u tablici umjesto kao json
 streamlit.dataframe(fruityvice_normalized)
