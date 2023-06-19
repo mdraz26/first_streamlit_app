98aa5b200f938509bf987fc4733bdf272a5b3644
mdraz26 <105057959+mdraz26@users.noreply.github.com>
2023-06-19 12:17:12 +0200

    Update streamlit_app.py
    added another function and a button to secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,19 @@ except URLError as e:
     streamlit.error()
 
 streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
+
+streamlit.header("The fruit load list contains:")
+#snowflake related function
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
+       return my_cur.fetchall()
+    
+#add a button to load the fruit    
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 fruit_choice_list = streamlit.text_input('What fruit would you like to add?','banana')
 streamlit.write('Thanks for adding:',fruit_choice_list)
